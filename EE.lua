e1f53b83c21b1a0f0778861101000a0da8dc4c5f
PretoMaxco <45157172+Peerapatllo@users.noreply.github.com>
2025-10-08 12:59:03 +0700

    Update EE.lua

--- EE.lua
+++ EE.lua
@@ -61,7 +61,7 @@ end
 
 local Blacklist = {"Wolf_E1", "Kangroo_E1", "Rhino_E1", "Lion_E1", "Gorilla_E1", "Seaturtle", "Okapi", "Needlefish", "Panther", "Butterflyfish"}
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players['Fiscostomath'].Character.HumanoidRootPart.CFrame
-task.wait(1)
+task.wait(3)
 for _,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Data.Pets:GetChildren()) do
 	if not table.find(Blacklist, v:GetAttribute("T")) then
 		game.ReplicatedStorage.Remote.CharacterRE:FireServer("Focus", v.Name)
@@ -70,5 +70,5 @@ for _,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Data.Pets:GetC
 		task.wait(0.3)
 	end
 end
-task.wait(10)
+task.wait(3)
 game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
